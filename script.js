76c13df6041d25d8c6c4c8c921a0b0fc4e26a605
julia <126582644+liadivensi@users.noreply.github.com>
2024-09-06 08:43:49 -0300

    ok

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
+
 {
-enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
-alternativas: [
+enunciado: " A Fórmula 1 está empenhada em tornar o esporte mais sustentável e reduzir a sua pegada de carbono. Uma das principais iniciativas inclui o desenvolvimento de novas tecnologias para carros mais ecológicos.",
+alternativas: A Fórmula 1 está planejando a introdução de combustíveis sintéticos e híbridos para reduzir a emissão de gases poluentes e promover a sustentabilidade no esporte.
+A Fórmula 1 continuará utilizando exclusivamente motores a combustão interna tradicionais sem qualquer mudança significativa para se tornar mais sustentável.
+
+ [
 {
-texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
-afirmacao: "afirmacaol"
-},
-{
-texto: "Denunciando atos de discriminação e apoiando políticas públicas inclusivas.",
-afirmacao: "afirmacao2"
-}
-]
-},
-{
-enunciado: "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
-alternativas: [
-{
-texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
-afirmacao: "afirmacao3"
+texto: "Em um esforço para se adaptar às crescentes preocupações ambientais, a Fórmula 1 está investindo pesadamente em tecnologias que promovem a sustentabilidade. Espera-se que as próximas gerações de carros de Fórmula 1 sejam movidas por combustível sustentável e tecnologias híbridas, com o objetivo de reduzir as emissões e o impacto ambiental do esporte.",
+afirmacao: "O futuro da Fórmula 1 está fortemente ligado ao desenvolvimento de tecnologias sustentáveis.
+
+"
 },
 {
 texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
